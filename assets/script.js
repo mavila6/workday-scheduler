0225b8ac0ce5856cedc937face9360f04982e308
Marco Avila <mavila6@email.arizona.com>
2021-06-06 23:13:44 -0700

    added for loop to go through hours of the day and save values to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
 //Added variable for moment.js timestamp as well as time format//
-const currentTime = moment().format("dddd, MMM Do YYYY");
+const currentDate = moment().format("dddd, MMM Do YYYY");
 //Appended currentTime variable to the p element in jumbotron with currentDay id//
-$("#currentDay").append(currentTime);
+$("#currentDay").append(currentDate);
 
+//Made an array for different hours in the workday and saved it in a variable//
+const timesArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+const input = $("#textEl");
+const save = $("#saveEl");
+
+for (let i=0; i < timesArr.length; i++) {
+    let hour = timesArr[i];
+
+    input.attr("id", hour);
+
+    let savedText = localStorage.getItem(hour);
+    input.val(savedText);
+
+    save.attr("date-time", hour);
+
+    save.on("click", function() {
+        localStorage.setItem(hour, input.val());
+    });
+};
